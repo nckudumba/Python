5be879fdb11f957bcf684ffdfd5824ad4ddfcb6d
nckudumba <mail2nitish616@gmail.com>
2025-07-27 11:20:18 +0530

    deleted 3 files

--- numeric.py
+++
@@ -1,7 +0,0 @@
-# print(max(1,2,3,4,5))
-
-# print(min(1,2,3,4,5))
-
-# print(abs(-200))
-
-# print(pow(2,5))
--- simple_program.py
+++
@@ -1,3 +0,0 @@
-print("hello world")
-a = input("Enter a value: ")
-print("Thank You!")
