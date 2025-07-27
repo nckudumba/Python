b6d81ca92b601ef173d2f4d906802cd6468e5719
nckudumba <mail2nitish616@gmail.com>
2025-07-27 11:16:17 +0530

    ADDED FILES

---
+++ Assignment-1/basic_mathematics.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a+b
+sub = a-b
+mult = a*b
+div = a/b
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mult)
+print("Division:", div)
