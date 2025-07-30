15a56b68824c09869739e3c1cd90e35d5c214121
nckudumba <mail2nitish616@gmail.com>
2025-07-31 01:50:36 +0530

    moved all the files!

--- for_2.py
+++
@@ -1,5 +0,0 @@
-#for loop 2
-
-for i in range(1,11):
-    print("a")
-
--- if.py
+++
@@ -1,8 +0,0 @@
-#if (test expression):
-    #statement1
-#statement2
-a = int(input("Enter a number: "))
-if a>5:
-    print("a is greater than 5")
-else:
-    print("none")
